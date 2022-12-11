fa9812f315057360ac613bfc347d5d13cca84d25
Sarvar <arifdjanovsarvar@gmail.com>
2022-12-11 17:07:33 +0500

    Phase 2
    Practical assignment completed

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# SpaceArtApp
- 
+# Project's Title
+### Space Art App
+
+## Project Description
+A simple application for Android-based smartphones, which shows the pictures, small titles and picture's authors.
+
+## How to install and Run the Project
+If you would like to install and modify the app:
+1. Download [Android Studio](https://developer.android.com/studio).
+2. Download the archive of the project or use a URL [link](https://github.com/OverLordN7/Weather_kotlin.git) to parse the project on your device  
+   using GitHub Desktop, Git console or other preferable tool.
+
+## How to use the Project
+If you would like to test the application:  
+Download the **SpaceArtApp.apk** file located in the **app_apk/** folder
+
+The application requires:
+* Min SDK: 28 (Android 9)
+* Target SDK: 32 (Android 11+)
+
+## Screenshots
+![]<img src=screenshots/screen1.png width = "300">![]<img src=screenshots/screen2.png width = "300">
+
+## Credits
+A great thanks to [@windchaser](https://www.freeimages.com/photo/big-apple-1056753), [@hisks](https://www.freeimages.com/photo/peacock-1169961), [@just4you](https://www.freeimages.com/photo/gerber-and-rose-2-1544099), [@adamci](https://www.freeimages.com/photo/cliff-over-indian-ocean-152086) for giving such wonderful photos.
+And [#AndroidBasic](https://developer.android.com/courses/android-basics-compose/unit-3) for such fascinating course.
+
+## License
+MIT License
+
+Copyright (c) 2022 Arifdjanov Sarvar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app/src/main/java/com/example/spaceartapp/MainActivity.kt
+++ app/src/main/java/com/example/spaceartapp/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.spaceartapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.spaceartapp.ui.theme.SpaceArtAppTheme
+
+private const val TAG= "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +33,126 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SpaceArtApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    SpaceArtAppTheme {
-        Greeting("Android")
+fun SpaceArtApp() {
+    var pictureNumber by remember {
+        mutableStateOf(0)
+    }
+
+    var imageRes: Int
+    var stringTitleRes: Int
+    var stringAuthorRes: Int
+
+    when(pictureNumber){
+        1 ->{
+            imageRes = R.drawable.picture2
+            stringTitleRes = R.string.picture_name_2
+            stringAuthorRes = R.string.picture_author_2
+        }
+        2 ->{
+            imageRes = R.drawable.picture3
+            stringTitleRes = R.string.picture_name_3
+            stringAuthorRes = R.string.picture_author_3
+        }
+        3 ->{
+            imageRes = R.drawable.picture4
+            stringTitleRes = R.string.picture_name_4
+            stringAuthorRes = R.string.picture_author_4
+        }
+        else ->{
+            imageRes = R.drawable.picture1
+            stringTitleRes = R.string.picture_name_1
+            stringAuthorRes = R.string.picture_author_1
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        ImageCard(imageRes)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        TextCard(stringTitleRes,stringAuthorRes,2022)
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Row() {
+            Button(onClick = {
+                if (pictureNumber == 3) pictureNumber=0 else pictureNumber++ }, modifier = Modifier.width(100.dp)) {
+                Text(text = "Previous")
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            Button(onClick = { if (pictureNumber == 0) pictureNumber=3 else pictureNumber-- }, modifier = Modifier.width(100.dp)) {
+                Text(text = "Next")
+
+            }
+        }
+        
     }
 }
+
+@Composable
+fun ImageCard(imageRes: Int, modifier: Modifier = Modifier){
+    Card(elevation = 10.dp) {
+        Box(
+            modifier = modifier
+                .width(350.dp)
+                .border(width = 3.dp, Color.Black, shape = RectangleShape)
+                .padding(all = 30.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+fun TextCard(
+    stringResTitle:Int,
+    stringResAuthor:Int,
+    year: Int,
+    modifier: Modifier = Modifier
+){
+    val paddingModifier  = Modifier.padding(10.dp)
+    Card(elevation = 10.dp, modifier = paddingModifier.width(300.dp)) {
+
+        Column(verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(all = 20.dp)) {
+
+            Text(text = stringResource(id = stringResTitle), fontSize = 18.sp)
+            Row() {
+                Text(text = stringResource(id = stringResAuthor), fontWeight = FontWeight.Bold)
+                Text(text = " ($year)")
+            }
+        }
+    }
+}
+
+
+
+fun nextPicture(currentPicture: Int):Int{
+    var temp = currentPicture
+    if (temp == 3){
+        return 0
+    } else {
+        return temp++
+    }
+}
+
+
+
+
+
